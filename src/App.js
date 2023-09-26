1292f579f81c8a15ea9998dc784072385d0b115c
rasha <rashataher1245@gmail.com>
2023-09-26 09:19:12 +0300

    Fixed some parts untill they match

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import "./style.css";
+import Headers from"./components/Header"
+import Contact from "./components/Contact";
+import Footer from "./components/footer"
+import ProjectSlider from "./components/ProjectSlider";
+import About from "./components/about"
+import Skills from "./components/skills"
+
 function App() {
   return (
     <div className="App">
-
+        <Headers/>
+        <About/>
+        <Skills/>
       <div className="project-container">
       <p className="title">Projects</p>
-      <ProjectSlider projects={projects} />
+      
       </div>
 
       <Contact/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import intuitive from "../Svgs/intuitive.svg";
+import responsive from "../Svgs/responsive.svg";
+import Dynamic from "../Svgs/dynamic.svg";
+import Fast from "../Svgs/clock.svg";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="PenBackround">
+        <div className="header">
+          <p>Home</p>
+          <p>About</p>
+          <p>Skills</p>
+          <p>Project</p>
+          <p>Contact</p>
+        </div>
+
+        <div className="TitleP">
+          <p className="name-title">
+            I am Shant Kel-Khacherian <br></br>
+          </p>
+          <p className="work-title">Web developer</p>
+        </div>
+      </div>
+      <section>
+        <div className="bule-continer">
+          <div className="Bluediv">
+            <img src={Fast} className="image"></img>
+          </div>
+          <p className="sub-titles">Fast</p>
+          <p>
+            Fast load times and lag<br></br>
+            free interaction my <br></br>
+            highes priority
+          </p>
+        </div>
+        <div className="bule-continer">
+          <div className="Bluediv">
+            <img src={responsive} className="image"></img>
+          </div>
+          <p className="sub-titles">Responsive</p>
+          <p>
+            My layouts will work<br></br>
+            on any device big or<br></br> small
+          </p>
+        </div>
+        <div className="bule-continer">
+          <div className="Bluediv">
+            <img src={intuitive} className="image"></img>
+          </div>
+          <p className="sub-titles">Itituative</p>
+          <p>
+            My layouts will work<br></br>
+            on any device big or <br></br>small
+          </p>
+        </div>
+        <div className="bule-continer">
+          <div className="Bluediv">
+            <img src={Dynamic} className="image"></img>
+          </div>
+          <p className="sub-titles">Dynamic</p>
+          <p>
+            Website don’thave to be specific.<br></br>I love making pages
+            <br></br> come to life
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default Header;
