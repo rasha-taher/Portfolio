d713c1320ff0cccc58997ec7a711358d871b0990
rasha <rashataher1245@gmail.com>
2023-10-02 15:14:17 +0300

    Fixed the responsive

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Footer from "./components/footer"
 import About from "./components/about"
 import Skills from "./components/skills"
 import ProjectsComponent from "./components/ProjectsComponent";
-import './style/a-responsive.css'
+
 import './style/about.css'
-import './style/a-responsive.css'
 import './style/App.css'
 import './style/header.css'
-import './style/r-responsive.css'
+import './style/style.css'
 import './style/responsive.css'
 import './style/skills.css'
-import './style/style.css'
 function App() {
   return (
     <div className="App">
