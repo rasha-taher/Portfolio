773f3d54ca39bdf589adc0df635b715ca37ee92b
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-10-02 11:58:05 +0300

    Add Skills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,26 +7,29 @@ import mongodb from "../Svgs/mongodb1.svg";
 import node from "../Svgs/node.svg";
 import database from "../Svgs/database1.svg";
 import html from "../Svgs/html1.svg";
+import javascript2 from "../Svgs/javascript2.svg";
+import css from "../Svgs/css.svg";
 
-const Skills= ()=>{
-    return( 
-        <div className="skill-section" id="skill">
-            <div className="skill1">
-                <p  className="title">Skills</p>
-                <img className="skill" src={html}></img>
-                
-                <img className="skill" src={figma}></img>
-                <img className="skill" src={github}></img>
-                <img className="skill" src={gitlab}></img>
-            </div>
-            <div className="skill2">
-            <img className="skill" src={javascript}></img>
-            <img className="skill" src={mongodb}></img>
-            <img className="skill" src={node}></img>
-            <img className="skill" src={database}></img>
-            </div>
-            <div>
-            </div>
-        </div>
-    )}
-export default Skills;    
+const Skills = () => {
+  return (
+    <div className="skill-section" id="skill">
+      <div className="skill1">
+        <p className="title">Skills</p>
+        <img className="skill" src={html}></img>
+        <img className="skill" src={javascript2}></img>
+        <img className="skill" src={figma}></img>
+        <img className="skill" src={github}></img>
+        <img className="skill" src={gitlab}></img>
+      </div>
+      <div className="skill2">
+        <img className="skill" src={css}></img>
+        <img className="skill" src={javascript}></img>
+        <img className="skill" src={mongodb}></img>
+        <img className="skill" src={node}></img>
+        <img className="skill" src={database}></img>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+export default Skills;
