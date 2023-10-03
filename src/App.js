d4ab218319eb8c2673408c30b2f42e446788478e
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-10-03 16:10:47 +0300

    Edit responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import Headers from"./components/Header"
+import Headers from "./components/Header";
 import Contact from "./components/Contact";
-import Footer from "./components/footer"
-import About from "./components/about"
-import Skills from "./components/skills"
+import Footer from "./components/footer";
+import About from "./components/about";
+import Skills from "./components/skills";
 import ProjectsComponent from "./components/ProjectsComponent";
 
-import './style/about.css'
-import './style/App.css'
-import './style/header.css'
-import './style/style.css'
-import './style/responsive.css'
-import './style/skills.css'
+import "./style/about.css";
+import "./style/App.css";
+import "./style/header.css";
+import "./style/style.css";
+import "./style/responsive.css";
+import "./style/skills.css";
 function App() {
   return (
     <div className="App">
-        <Headers/>
-        <About/>
-        <Skills/>
+      <Headers />
+      <About />
+      <Skills />
       <ProjectsComponent />
-      <Contact/>
-      <Footer/>
+      <Contact />
+      <Footer />
     </div>
   );
 }
